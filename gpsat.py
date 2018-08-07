c00fa1e09663e487dd697cf094da1573c4ed5265
SDX <realsdx0@gmail.com>
2018-08-07 18:39:07 +0530

    GPS test2

--- gpsat.py
+++ gpsat.py
@@ -1,15 +1,18 @@
 import serial
+import time
 
 SERIAL_PORT = "/dev/ttyUSB2"
 
 ser = serial.Serial(SERIAL_PORT, baudrate=9600, timeout=5)
-ser.write("AT\r")
+ser.write(str.encode("AT\r"))
+time.sleep(2)
 reply = ser.read(ser.inWritting())
 
 if "OK" in reply:
 	print("Setup OKAY.")
 
-	ser.write("AT+CGPS=1\r")
+	ser.write(str.encode("AT+CGPS=1\r"))
+	time.sleep(2)
 	reply = ser.read(ser.inWritting())
 	print(reply)
 else:
