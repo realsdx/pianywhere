f906a497975292c7d756d4ee7470208e830a444c
SDX <realsdx0@gmail.com>
2018-08-16 13:53:55 +0530

    Added logging

--- service/checkif.py
+++ service/checkif.py
@@ -3,6 +3,14 @@ import socket
 import fcntl
 import struct
 import commands as sp
+import logging
+
+logging.basicConfig(filename="InterfaceCheck.log",
+    format='%(asctime)s - %(levelname)s:%(funcName)s - %(message)s')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 
 def check(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,34 +19,40 @@ def check(ifname):
             s.fileno(),0x8915,
             struct.pack('256s', ifname[:15]))[20:24])
         if ip:
-            print "Device is up -IP %s" % ip
+            logger.info( "Device is up -IP %s" % ip)
             s.close()
             return True
         else:
             s.close()
-            print "Can't get the Device IP"
+            logger.error( "Can't get the Device IP")
 
     except IOError as e:
         if (e.errno == 19 ):
-            print "No Such device"
+            logger.warning("No Such device")
             s.close()
             return False
         if (e.errno == 99 ):
-            print "Interface is down"
+            logger.warning("Interface is down")
             s.close()
             return False
         else:
-            print e
+            logger.error(e.strerror)
             s.close()
 
 def check_net(ifname):
-    status,result = sp.getstatusoutput("ping -c1 -w2 -I " + str(ifname)+ " 8.8.8.8")
-    if status == 0:
-        print("TRUE")
-        return True
-    else:
-        print("System  is DOWN !")
-        return False
+    try:
+        status,result = sp.getstatusoutput("ping -c1 -w2 -I " + str(ifname)+ " 8.8.8.8")
+        if status == 0:
+            logger.info("Interface %s has a working internet." %(ifname))
+            return True
+        else:
+            logger.warning("Interface %s has no internet" %(ifname))
+            return False
+    except Exception as e:
+        logger.error(e.strerror)
 
-check_net('wlo1')
+check_net('eno12')
+check('eno12')
+check('eno1')
+check('wlo1')
     
